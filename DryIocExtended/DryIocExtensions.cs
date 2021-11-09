12c41500dadc26432427e5f00d4f432fb33dade8
hamedfathi <hamedfathi@outlook.com>
2021-11-09 07:21:49 +0330

    feat: functionalities added

---
+++ DryIocExtended/DryIocExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace DryIoc.Microsoft.DependencyInjection
+{
+    public static class DryIocExtensions
+    {
+        public static IHostBuilder UseDryIoc(this IHostBuilder hostBuilder, IServiceProviderFactory<IContainer> factory = null)
+        {
+            return hostBuilder.UseServiceProviderFactory(factory ?? new DryIocServiceProviderFactory());
+        }
+    }
+}
